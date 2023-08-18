361f2d999cb701a4fb72f5f04a63d6a84fff6a63
ARIESTANK <kaungkhantlin2332003@gmail.com>
2023-08-18 20:38:22 +0630

    first commit

---
+++ assignment.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('color');
+
+    button.addEventListener('click', function() {
+        let nameset = "aries";
+        let emailset = "ariestank9@gmail.com";
+        const name1 = document.getElementById('name').value;
+        const email1 = document.getElementById('email').value;
+        const password1 = document.getElementById('Password').value;
+        const repassword1 = document.getElementById('Repassword').value;
+
+        if (nameset !== name1 || emailset !== email1 || password1 !== repassword1) {
+            alert("Something Went Wrong");
+        } else {
+            alert("Successful");
+        }
+    });
+});
+
+
+function color(){
+    document.getElementById("color").style.backgroundColor="black";
+    document.getElementById("color").style.color="white";
+    
+}
+function colorless(){
+    document.getElementById("color").style.backgroundColor="transparent";
+    document.getElementById("color").style.color="black";
+    
+}
